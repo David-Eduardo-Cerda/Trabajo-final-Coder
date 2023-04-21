d3ad753d9c30606b702d69acbd6372e581fcf6b1
DavidCerda <david.celular@outlook.com>
2023-04-20 23:36:07 -0300

    migrando el modelo

---
+++ PrimeraApp/models.py
@@ -0,0 +1,23 @@
+from django.db import models
+
+class Curso(models.Model):
+
+    nombre = models.CharField(max_length=40)
+    comicion = models.IntegerField()
+
+class Estudiante(models.Model):
+    nombre = models.CharField(max_length=30)
+    apellitod = models.CharField(max_length=30)
+    email = models.EmailField()
+
+class Profesor(models.Model):
+    nombre = models.CharField(max_length=30)
+    apellitod = models.CharField(max_length=30)
+    email = models.EmailField()
+    profecion = models.CharField(max_length=30)
+
+class Entregables(models.Model):
+    nombre = models.CharField(max_length=30)
+    fechaDeEntrega = models.CharField(max_length=20)
+    entregado = models.BooleanField()
+    
